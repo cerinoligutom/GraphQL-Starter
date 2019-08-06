206e14d4049f4a78e62c893129235c99ebbe677e
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-08-06 19:10:32 +0800

    Added default jwt secret

--- src/config/jwt-options.ts
+++ src/config/jwt-options.ts
@@ -8,7 +8,7 @@ export interface IJwtOptions {
 export const jwtOptions: IJwtOptions = {
   issuer: process.env.JWT_ISSUER || 'Graphql Starter API',
   audience: process.env.JWT_AUDIENCE || 'GraphQL Starter API Consumer',
-  secretKey: process.env.JWT_SECRET || '',
+  secretKey: process.env.JWT_SECRET || 'jw+s3cr3+',
   expiresIn: '7d',
 };
 

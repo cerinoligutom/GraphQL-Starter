2cd1765081f6b662124d0c13a9e32fb892559f8b
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-09-10 02:42:25 +0800

    Added context type on custom gql shield rule

--- src/graphql-shield/rules/isAuthenticated.ts
+++ src/graphql-shield/rules/isAuthenticated.ts
@@ -1,7 +1,8 @@
 import { rule } from 'graphql-shield';
 import { AuthenticationError } from 'apollo-server-core';
+import { IGraphQLContext } from '@app/graphql';
 
-export const isAuthenticated = rule()(async (parent, args, ctx) => {
+export const isAuthenticated = rule()(async (parent, args, ctx: IGraphQLContext) => {
   if (ctx.userId === null) {
     return new AuthenticationError('Unauthenticated. Please try to log in.');
   }

53c78f6085a0c89324934cef48cea3f70176ad12
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-10-18 17:48:15 +0800

    Added a pro tip

--- src/graphql/resolvers/user/fullName.query.ts
+++ src/graphql/resolvers/user/fullName.query.ts
@@ -1,7 +1,9 @@
 import { GQL_UserResolvers } from 'graphql-resolvers';
+import { User } from '@app/db/models';
 
 export const fullName: GQL_UserResolvers['fullName'] = async parent => {
-  const name = [parent.firstName, parent.middleName, parent.lastName]
+  const user = parent as User;
+  const name = [user.firstName, user.middleName, user.lastName]
     .map(x => (x ? x.trim() : ''))
     .filter(x => x.trim())
     .join(' ');

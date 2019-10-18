1bee2ef906a5c683c667962f5df9fef61ff19089
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-10-18 17:40:28 +0800

    Add yupSchema to model

--- src/db/models/User.model.ts
+++ src/db/models/User.model.ts
@@ -1,7 +1,54 @@
 import { BaseModel } from './common/BaseModel';
+import * as yup from 'yup';
 
 export class User extends BaseModel {
   static tableName = 'users';
+
+  static yupSchema = {
+    firstName: yup
+      .string()
+      .min(2)
+      .trim()
+      .required(),
+
+    middleName: yup.string().nullable(),
+
+    lastName: yup
+      .string()
+      .min(2)
+      .required(),
+
+    username: yup
+      .string()
+      .min(4)
+      .max(32)
+      .required()
+      .test({
+        message: 'Username already exists.',
+        test: async (value: string) => {
+          const user = await User.query().findOne({
+            username: value,
+          });
+
+          return !user;
+        },
+      }),
+
+    email: yup
+      .string()
+      .email()
+      .required()
+      .test({
+        message: 'Email already exists.',
+        test: async (value: string) => {
+          const user = await User.query().findOne({
+            email: value,
+          });
+
+          return !user;
+        },
+      }),
+  };
 
   id!: string;
   username!: string;
--- src/graphql-shield/yup-validation-schemas/register.schema.ts
+++ src/graphql-shield/yup-validation-schemas/register.schema.ts
@@ -1,52 +1,15 @@
 import * as yup from 'yup';
 import { User } from '@app/db/models';
 
+const { firstName, middleName, lastName, username, email } = User.yupSchema;
+
 export const registerSchema = yup.object({
   input: yup.object({
-    firstName: yup
-      .string()
-      .min(2)
-      .trim()
-      .required(),
-
-    middleName: yup.string().nullable(),
-
-    lastName: yup
-      .string()
-      .min(2)
-      .required(),
-
-    username: yup
-      .string()
-      .min(4)
-      .max(32)
-      .required()
-      .test({
-        message: 'Username already exists.',
-        test: async (value: string) => {
-          const user = await User.query().findOne({
-            username: value,
-          });
-
-          return !user;
-        },
-      }),
-
-    email: yup
-      .string()
-      .email()
-      .required()
-      .test({
-        message: 'Email already exists.',
-        test: async (value: string) => {
-          const user = await User.query().findOne({
-            email: value,
-          });
-
-          return !user;
-        },
-      }),
-
+    firstName,
+    middleName,
+    lastName,
+    username,
+    email,
     password: yup
       .string()
       .required()

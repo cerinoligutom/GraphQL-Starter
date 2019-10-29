5aea3c7d47ecac32805132c1c5b69cb7209a02f2
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-10-30 01:34:17 +0800

    Modified yupRule to apply validated schema to args

--- src/graphql-shield/rules/yup-rule.ts
+++ src/graphql-shield/rules/yup-rule.ts
@@ -1,12 +1,17 @@
 import { UserInputError } from 'apollo-server-core';
 import * as yup from 'yup';
 import { rule } from 'graphql-shield';
+import * as _ from 'lodash';
 
 // tslint:disable-next-line: no-any
 export const yupRule = (schema: yup.Schema<any>) =>
   rule()(async (parent, args) => {
     try {
-      await schema.validate(args, { abortEarly: false });
+      const validatedArgs = await schema.validate(args, { abortEarly: false });
+
+      // Merge validatedArgs to args
+      _.assign(args, validatedArgs);
+
       return true;
     } catch (err) {
       return new UserInputError('Invalid form', err);

24f62843da38ff8ca2543d3c5fe7482e23048758
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-02-22 01:52:45 +0800

    Refactoring

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -3,15 +3,22 @@ import { env } from '@app/config/environment';
 import { logger } from '@app/utils';
 
 export const errorMiddleware = () => (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (!err) {
+    next();
+    return;
+  }
+
   // Log message error in our server's console
   logger.error(`[Error Middleware]: ${err}`);
   console.error(err.message);
 
   // All HTTP requests must have a response, so let's send back an error with its status code and message
   res.status(500).send({
-    errors: {
+    error: {
       message: env.isProduction ? 'Oops. Something went wrong.' : err.message,
       data: env.isProduction ? {} : err,
     },
   });
+
+  next(err);
 };

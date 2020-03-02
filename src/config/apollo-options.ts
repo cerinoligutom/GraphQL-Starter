e3dadac1eeb20f6df71e57e28708710474350fbf
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-03-02 18:01:22 +0800

    Refactoring - Apollo reads from the .env key ENGINE_API_KEY

--- src/config/apollo-options.ts
+++ src/config/apollo-options.ts
@@ -10,5 +10,5 @@ interface IApolloOptions {
 export const apolloOptions: IApolloOptions = {
   maxFileSize: +process.env.APOLLO_MAX_FILE_SIZE! || 10 * 1000 * 1000, // 10 MB
   maxFiles: +process.env.APOLLO_MAX_FILES! || 20,
-  engineApiKey: process.env.APOLLO_ENGINE_API_KEY,
+  engineApiKey: process.env.ENGINE_API_KEY,
 };

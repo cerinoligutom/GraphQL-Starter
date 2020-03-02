3df42b3849aa0518203c91fd90aa9a6c2358c001
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-03-03 04:10:16 +0800

    Added comment on "src/graphql/enums/index.ts"

--- src/graphql/enums/index.ts
+++ src/graphql/enums/index.ts
@@ -1,2 +1,6 @@
+// If you have enums that belongs to the "src/core" directory that you want to
+// expose to the GraphQL Schema, just export them directly here and it'll
+// be resolved as per configuration from "src/graphql/schema.ts".
+
 export * from './user-sort-field.enum';
 export * from './sort-direction.enum';

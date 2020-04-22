ffa43399dc2547cfae63981df531aaed25a79aa1
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-04-22 11:18:32 +0800

    Removed unused code

--- src/graphql/resolvers/common/node.interface.ts
+++ src/graphql/resolvers/common/node.interface.ts
@@ -1,5 +1,3 @@
-import { ApolloError } from 'apollo-server-core';
-
 export const NodeInterface = {
   // tslint:disable-next-line: no-any function-name
   __resolveType(obj: any, ctx: any, info: any): string {

21d1974e8d5c6c59202131d05ff0e58f4df9a8a7
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-09-05 01:35:04 +0800

    refactor: removed state validation on login schema
    This is not a concern for schema validation.
    
    State validations should be on a lower layer than this.

--- src/graphql-shield/yup-validation-schemas/login.schema.ts
+++ src/graphql-shield/yup-validation-schemas/login.schema.ts
@@ -1,20 +1,8 @@
 import * as yup from 'yup';
-import { UserModel } from '@app/db/models';
 
 export const loginSchema = yup.object({
   input: yup.object({
-    usernameOrEmail: yup
-      .string()
-      .required()
-      .test({
-        message: 'Username/Email does not exist.',
-        test: async (value: string) => {
-          const user = await UserModel.query().where('username', value).orWhere('email', value).first();
-
-          return !!user;
-        },
-      }),
-
+    usernameOrEmail: yup.string().required(),
     password: yup.string().min(8).max(32),
   }),
 });

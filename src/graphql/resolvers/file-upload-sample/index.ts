472bfa5ad39028d3a2945f422dda8bdb11af1cc7
Cerino O. Ligutom III <zeferinix@gmail.com>
2020-09-05 01:14:54 +0800

    refactor: file name casing

--- src/graphql/resolvers/file-upload-sample/index.ts
+++ src/graphql/resolvers/file-upload-sample/index.ts
@@ -1,6 +1,6 @@
 import { GQL_Resolvers } from 'graphql-resolvers';
-import { singleUploadResolver } from './singleUpload.mutation';
-import { multipleUploadResolver } from './multipleUpload.mutation';
+import { singleUploadResolver } from './single-upload.mutation';
+import { multipleUploadResolver } from './multiple-upload.mutation';
 
 const resolvers: GQL_Resolvers = {
   Mutation: {
--- src/graphql/resolvers/user/index.ts
+++ src/graphql/resolvers/user/index.ts
@@ -1,6 +1,6 @@
 import { GQL_Resolvers } from 'graphql-resolvers';
 import { usersResolver } from './users.query';
-import { fullNameResolver } from './fullName.query';
+import { fullNameResolver } from './full-name.query';
 
 const resolvers: GQL_Resolvers = {
   Query: {

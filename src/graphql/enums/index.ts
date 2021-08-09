d092f80350bd463abb988edfc93bea7778714c6b
Cerino Ligutom III <zeferinix@gmail.com>
2021-08-09 16:46:08 +0800

    docs: fix comments on graphql enum index barrel

--- src/graphql/enums/index.ts
+++ src/graphql/enums/index.ts
@@ -5,12 +5,12 @@
 // Do know that you can actually resolve these enums on their respective
 // module graphql index barrel and it would still work. If you prefer to resolve
 // it on the module level, make sure to map the enum in the `codegen.yml`
-// if you also want to get strict type checking.
+// if you also want to get strict types in generated code.
 
 // GraphQL Code Generator is currently configured to map enum values
 // to this file as seen on `codegen.yml` under the `enumValues` key.
 // Rationale being, I'd prefer for this file to grow rather than the
-// codegen config as the project get bigger over time.
+// codegen config as the project gets bigger over time.
 
 export { SortDirection } from '@/shared/constants';
 export { UserSortField } from '@/modules/user/constants/user-sort-field.enum';

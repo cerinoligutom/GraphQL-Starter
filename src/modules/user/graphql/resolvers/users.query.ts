1f0be66c882a26d29d006d709f9b0a7f367be04b
Cerino Ligutom III <zeferinix@gmail.com>
2021-10-30 14:20:22 +0800

    refactor: adjust imports

--- src/modules/user/graphql/resolvers/users.query.ts
+++ src/modules/user/graphql/resolvers/users.query.ts
@@ -1,6 +1,8 @@
-import { GQL_QueryResolvers, GQL_UserConnection, GQL_UserEdge, SortDirection, UserSortField } from '@/generated/graphql';
+import { GQL_QueryResolvers, GQL_UserConnection, GQL_UserEdge } from '@/generated/graphql';
 import { userFactory } from '../../factories/user.factory';
 import { getUsersUseCase } from '../../use-cases/get-users.use-case';
+import { SortDirection } from '@/shared/constants';
+import { UserSortField } from '../../constants/user-sort-field.enum';
 
 export const usersResolver: GQL_QueryResolvers['users'] = async (root, args, ctx) => {
   const result = await getUsersUseCase(

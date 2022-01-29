f4cb05735c155fd27fb1690ec1a4fdb62e5cb0fb
Cerino Ligutom III <zeferinix@gmail.com>
2022-01-29 22:58:38 +0800

    fix: only create id before inserting to db

--- src/db/models/common/base-model.ts
+++ src/db/models/common/base-model.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 import { UniqueID } from '@/shared/types';
+import { QueryContext } from 'objection';
 import { v4 as uuidV4 } from 'uuid';
 import { ObjectionModel } from './objection-model';
 
@@ -10,8 +11,11 @@ export class BaseModel extends ObjectionModel {
 
   id!: UniqueID;
 
-  constructor() {
-    super();
-    this.id = uuidV4();
+  $beforeInsert(queryContext: QueryContext) {
+    super.$beforeInsert(queryContext);
+
+    if (!this.id) {
+      this.id = uuidV4();
+    }
   }
 }

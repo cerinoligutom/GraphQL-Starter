4561827c79330b62a115ea1cb409f242a335044d
Cerino Ligutom III <zeferinix@gmail.com>
2022-05-24 22:28:15 +0800

    build: update dependencies (#40)
    * build: update deps
    
    * build: update ioredis
    
    * build: update husky
    
    * build: update commitlint
    
    * build: update supertokens
    
    * build: update tsconfig-paths
    
    * build: update knex to v2
    
    * build: update deps

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -9,8 +9,8 @@ interface IEnvironmentConfig {
     port: number;
   };
 
-  postgresConnectionUrl?: string;
-  redisConnectionUrl?: string;
+  postgresConnectionUrl: string;
+  redisConnectionUrl: string;
 }
 
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() ?? 'development';
@@ -22,12 +22,21 @@ export const env: IEnvironmentConfig = {
     environment: NODE_ENV,
     port: +process.env.APP_PORT! || 8080,
   },
-  postgresConnectionUrl: process.env.POSTGRES_CONNECTION_URL,
-  redisConnectionUrl: process.env.REDIS_CONNECTION_URL,
+  postgresConnectionUrl: process.env.POSTGRES_CONNECTION_URL!,
+  redisConnectionUrl: process.env.REDIS_CONNECTION_URL!,
 };
 
 // Environment defaults
 if (!isProduction) {
   env.postgresConnectionUrl ||= 'postgresql://postgres:password@db:5432/db';
   env.redisConnectionUrl ||= 'redis://redis';
+}
+
+// Environment Guards
+if (!env.postgresConnectionUrl) {
+  throw new Error('POSTGRES_CONNECTION_URL is required');
+}
+
+if (!env.redisConnectionUrl) {
+  throw new Error('REDIS_CONNECTION_URL is required');
 }

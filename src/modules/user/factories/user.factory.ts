26e679af3d017aeac04a7348ffb2585484d2418c
Cerino Ligutom III <zeferinix@gmail.com>
2023-06-06 18:19:35 +0800

    refactor: improve responses and schema validation

--- src/modules/user/factories/user.factory.ts
+++ src/modules/user/factories/user.factory.ts
@@ -1,5 +1,5 @@
 import { GQL_User } from '@/generated/graphql/index.js';
-import { ResponseUserFull, ResponseUserSimple } from '../responses/user.response.js';
+import { ResponseUserFull, ResponseUserFullSchema, ResponseUserSimple, ResponseUserSimpleSchema } from '../responses/user.response.js';
 import { Selectable } from 'kysely';
 import { User } from '@/db/types.js';
 
@@ -12,6 +12,18 @@ import { User } from '@/db/types.js';
 // Make sure to set the return type of the factory functions!
 
 function createGQLUser(user: Selectable<User>): GQL_User {
+  /**
+   * Using Zod Schemas to validate GQL responses, I think, is up to you or your team.
+   * The GraphQL Scalar types usually handles this for us so depending on your
+   * preference, you can either use Zod Schemas or not.
+   *
+   * Take for example, the `Int` Scalar only accepts integer values but does not
+   * restrict values to be within a certain range.
+   * Some teams prefer making use of GraphQL Directives (https://www.apollographql.com/docs/apollo-server/schema/directives/)
+   * to achieve this, some prefer doing it on this level using whatever schema validation
+   * tool the team prefers (for this boilerplate, it's Zod.).
+   */
+
   return {
     id: user.id,
     firstName: user.firstName,
@@ -32,21 +44,11 @@ function createGQLUser(user: Selectable<User>): GQL_User {
 }
 
 function toFullResponse(user: Selectable<User>): ResponseUserFull {
-  return {
-    id: user.id,
-    firstName: user.firstName,
-    middleName: user.middleName,
-    lastName: user.lastName,
-    email: user.email,
-    createdAt: user.createdAt.toISOString(),
-    updatedAt: user.updatedAt.toISOString(),
-  };
+  return ResponseUserFullSchema.parse(user);
 }
 
 function toSimpleResponse(user: Selectable<User>): ResponseUserSimple {
-  return {
-    id: user.id,
-  };
+  return ResponseUserSimpleSchema.parse(user);
 }
 
 export const userFactory = {
--- src/modules/user/responses/user.response.ts
+++ src/modules/user/responses/user.response.ts
@@ -1,13 +1,26 @@
-export type ResponseUserFull = {
-  id: string;
-  firstName: string;
-  middleName: string | null;
-  lastName: string;
-  email: string;
-  createdAt: string;
-  updatedAt: string;
-};
+import { z } from 'zod';
+import { UserSchema } from '@/db/schema/index.js';
 
+export const ResponseUserFullSchema = z
+  .object({
+    id: UserSchema.shape.id,
+    firstName: UserSchema.shape.firstName,
+    middleName: UserSchema.shape.middleName,
+    lastName: UserSchema.shape.lastName,
+    email: UserSchema.shape.email,
+    createdAt: UserSchema.shape.createdAt,
+    updatedAt: UserSchema.shape.updatedAt,
+  })
+  .extend({
+    // We need the date objects to be serialized but in valid datetime format.
+    createdAt: z.string().datetime(),
+    updatedAt: z.string().datetime(),
+  });
+export type ResponseUserFull = z.infer<typeof ResponseUserFullSchema>;
+
+export const ResponseUserSimpleSchema = z.object({
+  id: UserSchema.shape.id,
+});
 export type ResponseUserSimple = {
   id: string;
 };
